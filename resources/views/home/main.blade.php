23c5e3f5fb7b8ccd556549207a43645ac7fc30d8
Maraiz <Anakbudiman01@gmail.com>
2024-06-30 04:21:47 +0700

    istirahat dly

--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -450,6 +450,12 @@
 </section>
 {{-- kontak penutup --}}
 
+    <!-- Footer -->
+    <section id="footer">
+        @include('home.footer')
+    </section>
+    {{-- footer Penutup --}}
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
         </script>
